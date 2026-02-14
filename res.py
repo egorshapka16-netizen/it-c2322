27a1aab56077740847475aadb57ac2ee00fd8e58
egorshapka16-netizen <egorshapka16@gmail.com>
2026-02-14 11:44:54 +0200

    Add files via upload

---
+++ res.py
@@ -0,0 +1,19 @@
+result = []
+
+def divider(a, b):
+    if a < b:
+        raise ValueError("a < b")
+    if b > 100:
+        raise IndexError("b > 100")
+    return a / b
+
+data = {10: 2, 2: 5, "123": 4, 18: 0, (): 15, 8: 4}
+
+for key in data:
+    try:
+        res = divider(key, data[key])
+        result.append(res)
+    except Exception as e:
+        print(f"Помилка для key={key}, value={data[key]} → {type(e).__name__}: {e}")
+
+print("Результат:", result)
